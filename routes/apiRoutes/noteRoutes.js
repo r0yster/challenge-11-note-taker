6290efe5439e47691fd4f5b1c5be88619eb78b8f
royster <rcantu786@gmail.com>
2021-06-05 10:40:19 -0500

    finalized post function to add notes

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { createNewNote } = require('../../lib/note')
+const { createNewNote } = require('../../lib/note');
 const { notes } = require('../../db/db.json');
 
 // get /api/notes should read the db.json file
@@ -9,10 +9,9 @@ router.get('/notes', (req, res) => {
 
 // post /api/notes should receive a new note to save on the request body
 // add to db.json and return the new note to the client
-router.post('/api/notes', (req, res) => {
-    req.body.id = notes.length.toString();
-    const note = createNewNote(req.body, notes);
-    res.json(note);
+router.post('/notes', (req, res) => {
+    const newNote = createNewNote(req.body, notes);
+    res.json(newNote);
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
+const app = express();
 
 const PORT = process.env.PORT || 3001;
-const app = express();
-const apiRoutes = require('./routes/apiRoutes');
+const apiRoutes = require('./routes/apiRoutes/noteRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-// app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
 
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
