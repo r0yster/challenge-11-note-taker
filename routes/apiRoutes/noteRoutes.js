1ee3f78f8bcad434e3d1eddc1e76d8ca420380c4
royster <rcantu786@gmail.com>
2021-06-05 11:30:51 -0500

    delete functionality added

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,13 +1,12 @@
 const router = require('express').Router();
 
-const { createNewNote } = require('../../lib/note');
+const { createNewNote, deleteNote } = require('../../lib/note');
 const { notes } = require('../../db/db.json');
 
 // get /api/notes should read the db.json file
 router.get('/notes', (req, res) => {
 
     res.json(notes);
-
 });
 
 // post /api/notes should receive a new note to save on the request body
@@ -18,7 +17,14 @@ router.post('/notes', (req, res) => {
 
     const newNote = createNewNote(req.body, notes);
     res.json(newNote);
-    
+});
+
+// delete /api/notes/:id should receive a query parameter containgin the id
+// of a note to delete
+router.delete('/notes/:id', (req, res) => {
+
+    let noteId = req.param.id;
+    res.json(deleteNote(noteId, notes));    
 });
 
 module.exports = router;
